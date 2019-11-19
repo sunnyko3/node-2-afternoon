d806fdb78d499f70d1c2b49d8c770160af374edf
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-11-19 16:03:23 -0700

    updated tuesday project

--- server/index.js
+++ server/index.js
@@ -1,11 +1,11 @@
 const express = require('express');
 const app = express();
 const mc = require('./controllers/messages_controller');
-const messagesBaseUrl = "/api/messages";
-
 
 app.use(express.json());
+app.use(express.static(__dirname + '/../public/build'));
 
+const messagesBaseUrl = "/api/messages";
 app.post(messagesBaseUrl, mc.create);
 app.get(messagesBaseUrl, mc.read);
 app.put(`${messagesBaseUrl}/:id`, mc.update);
