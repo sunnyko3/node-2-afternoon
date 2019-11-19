e9c93711043a453ca4d470f0e46d6e1fc90ebeab
sunnyko3 <57108786+sunnyko3@users.noreply.github.com>
2019-11-19 15:08:06 -0700

    tuesday afternoon

---
+++ server/index.js
@@ -0,0 +1,18 @@
+const express = require('express');
+const app = express();
+const mc = require('./controllers/messages_controller');
+const messagesBaseUrl = "/api/messages";
+
+
+app.use(express.json());
+
+app.post(messagesBaseUrl, mc.create);
+app.get(messagesBaseUrl, mc.read);
+app.put(`${messagesBaseUrl}/:id`, mc.update);
+app.delete(`${messagesBaseUrl}/:id`, mc.delete);
+
+
+const port = 3001;
+app.listen(port, () => {
+    console.log(`Server listening on port ${port}`);
+});
